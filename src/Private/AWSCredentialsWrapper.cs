20e6400e743e5a28d20b566cb749c7107047ad1c
Carl Sjoeholm <carl.sjoeholm@myob.com>
2024-12-12 16:00:20 +1100

    add ctor of AWSCredentialsWrapper to handle IAmazonSecurityTokenService, roleArn and roleSessionName add GetAssumedRoleCredentials change method GetCredentials and override GetCredentialsAsync

--- src/Private/AWSCredentialsWrapper.cs
+++ src/Private/AWSCredentialsWrapper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Amazon.Runtime;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
 
 namespace AwsSignatureVersion4.Private
 {
@@ -9,13 +12,76 @@ namespace AwsSignatureVersion4.Private
     /// </summary>
     public class AWSCredentialsWrapper : AWSCredentials
     {
-        private readonly ImmutableCredentials credentials;
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(true, nameof(credentials))]
+#endif
+        private readonly ImmutableCredentials? credentials;
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(true, nameof(securityTokenService), nameof(roleArn), nameof(roleSessionName))]
+#endif
+        private readonly IAmazonSecurityTokenService? securityTokenService;
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(true, nameof(securityTokenService), nameof(roleArn), nameof(roleSessionName))]
+#endif
+        private readonly string? roleArn;
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(true, nameof(securityTokenService), nameof(roleArn), nameof(roleSessionName))]
+#endif
+        private readonly string? roleSessionName;
 
         public AWSCredentialsWrapper(ImmutableCredentials credentials)
         {
             this.credentials = credentials ?? throw new ArgumentNullException(nameof(credentials));
         }
 
-        public override ImmutableCredentials GetCredentials() => credentials;
+        public AWSCredentialsWrapper(IAmazonSecurityTokenService securityTokenService, string roleArn, string roleSessionName)
+        {
+            this.securityTokenService = securityTokenService ?? throw new ArgumentNullException(nameof(securityTokenService));
+            this.roleArn = roleArn ?? throw new ArgumentNullException(nameof(roleArn));
+            this.roleSessionName = roleSessionName ?? throw new ArgumentNullException(nameof(roleSessionName));
+        }
+
+        private async Task<ImmutableCredentials> GetAssumedRoleCredentials()
+        {
+            if (securityTokenService is not null && !string.IsNullOrEmpty(roleArn) && !string.IsNullOrEmpty(roleSessionName))
+            {
+                var assumeRoleRequest = new AssumeRoleRequest
+                {
+                    RoleArn = roleArn,
+                    RoleSessionName = roleSessionName
+                };
+
+                var assumeRoleResponse = await securityTokenService
+                    .AssumeRoleAsync(assumeRoleRequest)
+                    .ConfigureAwait(false);
+
+                return new ImmutableCredentials(
+                    assumeRoleResponse.Credentials.AccessKeyId,
+                    assumeRoleResponse.Credentials.SecretAccessKey,
+                    assumeRoleResponse.Credentials.SessionToken);
+            }
+
+            throw new InvalidOperationException("Cannot assume role without STS client, role ARN, and role session name.");
+        }
+
+        public override ImmutableCredentials GetCredentials()
+        {
+            if (credentials is not null)
+            {
+                return credentials;
+            }
+
+            return GetAssumedRoleCredentials().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        public override async Task<ImmutableCredentials> GetCredentialsAsync()
+        {
+            if (credentials is not null)
+            {
+                return await Task.FromResult(credentials);
+            }
+
+            return await GetAssumedRoleCredentials();
+        }
     }
 }
